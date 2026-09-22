9a7cd213a4f9d3436cd0e445fa81f1603fa5c5ae
kauavnDev <kauavigilofc@gmail.com>
2026-09-22 12:53:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const promptDeSistema = `Você é um administrador do servidor. Nossa comunidade
 
 // 5. Configurando o cérebro com as instruções base
 const model = genAI.getGenerativeModel({ 
-  model: "gemini-1.5-flash",
+  model: "gemini-1.5-pro",
   systemInstruction: promptDeSistema
 });
 
